333f73624a56ca9a2fbcfe31b9dad998c3ba5330
amanfangeria980 <amanfangeria980@gmail.com>
2022-08-03 18:46:40 +0530

    Duplicate in Array.cpp added

---
+++ Arrays/DuplicateInArray.cpp
@@ -0,0 +1,14 @@
+// Problem Link: https://bit.ly/3dm6bdZ
+
+/* int findDuplicate(vector<int> &arr) 
+{
+    int sum=0;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    return(sum-(n*(n-1)/2));
+	
+} */
+
+// Logic applied here is sum of array elements-sum of natural nos.
