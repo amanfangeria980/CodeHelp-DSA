c0f94a9a37d8b7c27844c9477a841a65e7af9624
amanfangeria980 <amanfangeria980@gmail.com>
2022-08-03 18:56:22 +0530

    some minor changes

--- Arrays/DuplicateInArray.cpp
+++ Arrays/DuplicateInArray.cpp
@@ -12,3 +12,34 @@
 } */
 
 // Logic applied here is sum of array elements-sum of natural nos.
+
+/* 
+int findDuplicate(vector<int> &arr) 
+{
+    int res=0;
+    for(int i=0;i<arr.size();i++){
+        res=(res^arr[i]^i);
+    }
+    return res;
+}
+*/
+
+//In this solution the logic is,
+
+/* Reason for xor with “i”:-
+
+ 
+
+      for first loop:-     for(int i=0;i<arr.size();i++)
+
+                             ans =ans^arr[I];
+
+      “ans” will contain all the elements except for the duplicate           because that cancelled out
+
+ 
+
+     for second loop:-    for(int i=1;i<arr.size();i++)
+
+                        ans =ans^i;
+
+    as “ans” from first case contains 1 to (n-1) except one integer in this range  by doing xor of this “ans” with 1 to (n-1) will cancel out all the integers in “ans” but one that was missing in this range will remain and that is  our answer for this problem */
